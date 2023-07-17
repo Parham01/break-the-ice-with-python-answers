7d7ba9d0e10af34d250ef4e15eb52203c65bd3f3
parham01 <parham.jafari81@gmail.com>
2023-07-17 04:56:32 +0330

    first day done! second day almost...

--- Day1.py
+++ Day1.py
@@ -1,4 +1,10 @@
-# Question 1
+'''
+Write a program which will find all such numbers which are divisible by 7 but are 
+not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained
+should be printed in a comma-separated sequence on a single line.
+'''
+
+
 def divisible_by_7():
     for i in range(2000, 3201):
         if i % 7 == 0 and i % 5 != 0:
@@ -7,8 +13,13 @@ def divisible_by_7():
 
 divisible_by_7()
 
+'''
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+'''
 
-# Question 2
+
 def factorial(num):
     fact = 1
     for i in range(1, num + 1):
@@ -19,8 +30,13 @@ def factorial(num):
 given_number1 = int(input("\nenter a number: "))
 factorial(given_number1)
 
+'''
+With a given integral number n, write a program to generate a dictionary that contains (i, i x i) 
+such that is an integral number between 1 and n (both included). 
+and then the program should print the dictionary
+'''
 
-# Question 3
+
 def numbers_dictionary(num):
     dictionary = {}
     for i in range(1, num + 1):
---
+++ Day2.py
@@ -0,0 +1,110 @@
+from math import sqrt
+
+'''
+Question 4:
+Write a program which accepts a sequence of comma-separated numbers
+from console and generate a list and a tuple which contains every number
+'''
+
+
+def generate_list_tuple(csn):
+    numbers_list = csn.split(',')
+    numbers_tuple = tuple(numbers_list)
+    print(numbers_list)
+    print(numbers_tuple)
+
+
+comma_separated_input = input()
+generate_list_tuple(comma_separated_input)
+
+'''
+Question 5:
+Define a class which has at least two methods:
+    getString: to get a string from console input
+    printString: to print the string in upper case.
+
+Also please include simple test function to test the class methods.
+'''
+
+
+class UpperString:
+    def __init__(self):
+        self.str = ""
+
+    def getstring(self):
+        self.str = input()
+
+    def printstring(self):
+        print(self.str.upper())
+
+
+string1 = UpperString()
+string1.getstring()
+string1.printstring()
+
+'''
+Question 6:
+Write a program that calculates and prints the value according to the given formula:
+
+Q = Square root of [(2 * C * D)/H]
+
+Following are the fixed values of C and H:
+C is 50. H is 30.
+
+D is the variable whose values should be input to your program in a comma-separated sequence.
+'''
+
+
+def calculate_sqrt(csn):
+    c = 50
+    h = 30
+    for d in csn.split(','):
+        answer = sqrt((2 * c * int(d)) / h)
+        print(round(answer), end=',')
+
+
+cs_input = input()
+
+calculate_sqrt(cs_input)
+
+'''
+Question 7:
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array.
+The element value in the i-th row and j-th column of the array should be i _ j.
+
+Note: i=0,1.., X-1; j=0,1,¡Y-1. Suppose the following inputs are given to the program: 3,5
+'''
+
+
+def two_dimensional_array(li):
+    array = []
+    for i in range(int(li[0])):
+        array.append([])
+        for j in range(int(li[1])):
+            array[i].append(i * j)
+    print(array)
+
+
+dimensions = input().split(',')
+two_dimensional_array(dimensions)
+
+'''
+Question 8:
+Write a program that accepts a comma separated sequence of words as input and 
+prints the words in a comma-separated sequence after sorting them alphabetically.
+'''
+
+
+def string_sorting(str_list):
+    sorted_list = sorted(str_list.split(','))
+    print(','.join(sorted_list))
+
+
+strings = input()
+string_sorting(strings)
+
+'''
+Question 9:
+Write a program that accepts sequence of lines as input and prints 
+the lines after making all characters in the sentence capitalized.
+'''
